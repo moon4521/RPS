1652430db25ab9b74eec5f28deb5fb312748251b
moonstair <andie4195@gmail.com>
2024-10-03 15:20:53 +0700

    First RNG done

--- RPSscript.js
+++ RPSscript.js
@@ -1,1 +1,20 @@
-console.log('abc');
+function getComputerChoice() {
+    
+    let result = '';
+    let rng = Math.floor(Math.random() * 3 + 1);
+
+    if (rng === 1) {
+        result = 'rock';
+    } else if (rng === 2) {
+        result = 'paper';
+    } else if (rng === 3) {
+        result = 'scissors';
+    }
+
+    return result;
+}
+
+function getHumanChoice() {
+    
+}
+
