fa7925634490f51bc11a2f7a1a1aeb3fdafc4465
moonstair <andie4195@gmail.com>
2024-10-08 10:24:13 +0700

    DONE RPS

--- RPSscript.js
+++ RPSscript.js
@@ -29,22 +29,22 @@ function playRound(humanChoice, computerChoice) {
         result = `It\'s a draw. Human score is ${humanScore}. Computer score is ${computerScore}.`;
     } else if (humanChoice === 'rock' && computerChoice === 'paper') {
         computerScore++;
-        result = `You lose. PC paper beats Rock. Human score is ${humanScore}. Computer score is ${computerScore}.`;
+        result = `PC won. PC paper beats human rock. Human score is ${humanScore}. Computer score is ${computerScore}.`;
     } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
         humanScore++;
-        result = `You won. Rock beats PC scissors. Human score is ${humanScore}. Computer score is ${computerScore}.`;
+        result = `You won. Human rock beats PC scissors. Human score is ${humanScore}. Computer score is ${computerScore}.`;
     } else if (humanChoice === 'paper' && computerChoice === 'rock') {
         humanScore++;
-        result = `You won. Paper beats PC rock. Human score is ${humanScore}. Computer score is ${computerScore}.`;
+        result = `You won. Human paper beats PC rock. Human score is ${humanScore}. Computer score is ${computerScore}.`;
     } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
         computerScore++;
-        result = `You lose. PC scissors beats paper. Human score is ${humanScore}. Computer score is ${computerScore}.`;
+        result = `PC won. PC scissors beats human paper. Human score is ${humanScore}. Computer score is ${computerScore}.`;
     } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
         computerScore++;
-        result = `You lose. PC rock beats scissors. Human score is ${humanScore}. Computer score is ${computerScore}.`;
+        result = `PC won. PC rock beats human scissors. Human score is ${humanScore}. Computer score is ${computerScore}.`;
     } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
         humanScore++;
-        result = `You won. Scissors beats PC paper. Human score is ${humanScore}. Computer score is ${computerScore}.`;
+        result = `You won. Human scissors beats PC paper. Human score is ${humanScore}. Computer score is ${computerScore}.`;
     }
     return result;
 }
@@ -52,6 +52,7 @@ function playRound(humanChoice, computerChoice) {
 function playGame(n) {
 
     let finalResult = '';
+    
     if (n === 0) {
         if (humanScore === computerScore) {
             finalResult = `Final Result: It\'s a draw. Human is ${humanScore}. Computer is ${computerScore}.`;
@@ -61,7 +62,7 @@ function playGame(n) {
             finalResult = `Final Result: Human lose (${humanScore}). Computer won (${computerScore}).`;
         }
         return console.log(finalResult);
-    }
+    }  
     console.log(playRound(getHumanChoice(), getComputerChoice()));
     playGame(n-1);
 }
